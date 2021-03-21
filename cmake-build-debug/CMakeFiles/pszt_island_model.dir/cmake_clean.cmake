cd2473e65d45a0ba11fd60458799610de75e5528
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-03-21 11:45:27 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/pszt_island_model.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pszt_island_model.dir/main.cpp.o"
+  "pszt_island_model"
+  "pszt_island_model.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pszt_island_model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
