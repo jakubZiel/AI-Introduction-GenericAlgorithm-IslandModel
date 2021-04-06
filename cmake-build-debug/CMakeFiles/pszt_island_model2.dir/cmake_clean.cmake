91686dfd402fea7ea448f6f35115834ad7308e9d
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-04-06 17:49:15 +0200

    debug_files

---
+++ cmake-build-debug/CMakeFiles/pszt_island_model2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pszt_island_model2.dir/generation/New_Population_Factory.cpp.o"
+  "CMakeFiles/pszt_island_model2.dir/main.cpp.o"
+  "CMakeFiles/pszt_island_model2.dir/proto.cpp.o"
+  "pszt_island_model2"
+  "pszt_island_model2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pszt_island_model2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/island-model-algorithm/Island_model_evolution.cpp.o"
+  "CMakeFiles/test.dir/std-algorithm/Evolution.cpp.o"
+  "CMakeFiles/test.dir/std-algorithm/evolutionary_operations.cpp.o"
+  "CMakeFiles/test.dir/structures/structures.cpp.o"
+  "CMakeFiles/test.dir/test/main.cpp.o"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
